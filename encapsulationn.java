7840aae9e6ad02a7c6dbc1dc851c55a5912a7216
Sangavi1930 <sangaviram2004@gmail.com>
2026-04-07 09:50:33 +0530

    new

---
+++ encapsulationn.java
@@ -0,0 +1,27 @@
+class Student {
+    private String name;
+    private int age;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if(age > 0) {
+            this.age = age;
+        }
+    }
+}
+public class encapsulationn {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Sangavi");
+        s.setAge(20);
+        System.out.println("Name: " + s.getName());
+        System.out.println("Age: " + s.getAge());
+    }
+}
